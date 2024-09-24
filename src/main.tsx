56211e220653a4ecc4f1d41d69d3177321325602
ehsan <1ehsan.rezaee1@gmail.com>
2024-09-24 10:44:34 +0330

    [FEATURE] cleaning the Toast Component functionality

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import App from '@/App'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import ToastProvider from "@/Toast/ToastProvider";
-import "./Toast/index.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
